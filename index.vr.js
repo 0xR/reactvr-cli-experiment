9847efd41a619457c44e048091cb605bebe684cf
Mike Woudenberg <mwoudenberg@xebia.com>
2017-11-03 09:54:42 +0100

    Adds live camera and arrow asset

--- index.vr.js
+++ index.vr.js
@@ -1,34 +1,39 @@
-import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import React from "react";
+import { AppRegistry, asset, LiveEnvCamera, Model, Text, View } from "react-vr";
 
 export default class reactvr_cli_experiment extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
+        <LiveEnvCamera />
         <Text
           style={{
-            backgroundColor: '#777879',
+            backgroundColor: "#777879",
             fontSize: 0.8,
-            fontWeight: '400',
+            fontWeight: "400",
             layoutOrigin: [0.5, 0.5],
             paddingLeft: 0.2,
             paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
+            textAlign: "center",
+            textAlignVertical: "center",
+            transform: [{ translate: [0, 0, -3] }]
+          }}
+        >
           hello
         </Text>
+
+        <Model
+          source={{
+            obj: asset("model.obj"),
+            mtl: asset("materials.mtl")
+          }}
+        />
       </View>
     );
   }
-};
+}
 
-AppRegistry.registerComponent('reactvr_cli_experiment', () => reactvr_cli_experiment);
+AppRegistry.registerComponent(
+  "reactvr_cli_experiment",
+  () => reactvr_cli_experiment
+);
